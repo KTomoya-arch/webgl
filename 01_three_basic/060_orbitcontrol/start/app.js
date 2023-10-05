2bfc726653707ddba3c1f60508c55735fc52c6b9
KTomoya-arch <kato.eng.dao@gmail.com>
2023-10-05 23:51:55 +0900

    fix

--- 01_three_basic/060_orbitcontrol/start/app.js
+++ 01_three_basic/060_orbitcontrol/start/app.js
@@ -3,6 +3,7 @@
  * https://threejs.org/
  */
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 init();
 async function init() {
@@ -19,25 +20,28 @@ async function init() {
   document.body.appendChild(renderer.domElement);
 
   // const geometry = new THREE.BoxGeometry();
-  const geometry = new THREE.PlaneGeometry(32, 18);
+  // const geometry = new THREE.PlaneGeometry(32, 18);
   // const geometry = new THREE.SphereGeometry();
-  // const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
+  const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
   const texLoader = new THREE.TextureLoader();
   const texture2 = await texLoader.loadAsync("/img/output2.jpg");
-  const material = new THREE.MeshBasicMaterial({ map: texture2 });
-  
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
 
+  const control = new OrbitControls(camera, renderer.domElement);
+  control.enableDamping = true;
+  control.autoRotate = true;
   camera.position.z = 30;
 
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
-    // console.log(i++);
+    console.log("天鳳");
     // cube.rotation.x = cube.rotation.x + 0.01;
     // cube.rotation.y += 0.01;
-
+    control.update();
     renderer.render(scene, camera);
   }
 
